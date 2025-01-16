be5ea9e96fb62285ade8e0583d0bd4a46e42f51a
lucastotino <lucas_totino@hotmail.com>
2025-01-16 13:34:14 -0300

    Atualização Login

--- Thot/Repositorio/IUsuarioRepositorio.cs
+++ Thot/Repositorio/IUsuarioRepositorio.cs
@@ -4,6 +4,7 @@ namespace Thot.Repositorio
 {
     public interface IUsuarioRepositorio
     {
+        UsuarioModel BuscarPorLogin(string login);
         UsuarioModel ListarPorId(int id);
         List<UsuarioModel> BuscarTodos();
         UsuarioModel Adicionar(UsuarioModel usuario);
