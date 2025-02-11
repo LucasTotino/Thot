2cb73ae886e0584407caef5a324e89242498cdd3
lucastotino <lucas_totino@hotmail.com>
2025-02-10 22:44:07 -0300

    Adição de abas

--- Thot/Controllers/ProdutoController.cs
+++ Thot/Controllers/ProdutoController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Thot.Filters;
 using Thot.Models;
 using Thot.Repositorio;
 
 namespace Thot.Controllers
 {
+    [UsuarioLogado]
     public class ProdutoController : Controller
     {
         private readonly IProdutoRepositorio _produtoRepositorio;
